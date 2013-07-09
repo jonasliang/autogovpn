8c32392eb6b2899f55c2e99b77f63ea2f7f52183
Jonas Liang <liangzijie@gmail.com>
2013-07-09 21:04:43 +0800

    实现domain_match

--- src/module/domain_table.c
+++ src/module/domain_table.c
@@ -4,6 +4,7 @@
 #include <linux/moduleparam.h>
 #include <linux/list.h>
 #include <linux/mm.h>
+#include <linux/crc32c.h>
 #include "domain_table.h"
 #include "agv_debug.h"
 
@@ -25,6 +26,11 @@ struct domain_htable {
 };
 
 static struct domain_htable* s_domain_htable = NULL;
+
+inline static size_t domaincrc2bucket(uint32_t domain_crc, size_t htable_size)
+{
+	return domain_crc % htable_size;  // could be optimized
+}
 
 /*
  * @brief: alloc_domain_htable - 初始化域名哈希表及内部结构
@@ -62,6 +68,11 @@ _vfailed:
 	goto _done;
 }
 
+/*
+ * @brief: free_domain_htable - 释放域名哈希表
+ * @param: pdhtable [in | out] 域名哈希表二级指针，释放后会置空
+ * @return: void
+ */
 static void free_domain_htable(struct domain_htable** pdhtable)
 {
 	int i = 0;
@@ -88,7 +99,7 @@ static void free_domain_htable(struct domain_htable** pdhtable)
 }
 
 /*
- * @brief: 域名配置下发函数，更新域名列表
+ * @brief: domain_table_load - 域名配置下发函数，更新域名列表
  * @param: dlist [in] 由应用层下发的配置列表
  * @return: 成功返回0；失败返回<0
  */
@@ -104,7 +115,8 @@ int domain_table_load(const struct domain_list_st* dlist)
 	AGV_CHECK_AND_GO(!newdht, done, "alloc new domain_bucket failed!\n");
 
 	for (i = 0; i < dlist->m_size; ++i)	{
-		size_t index = dlist->m_domains_crc[i] % s_domain_htable_size; // could be optimized
+		size_t index = domaincrc2bucket(dlist->m_domains_crc[i], 
+			s_domain_htable_size);
 		struct domain_node* newnode = kmalloc(sizeof(struct domain_node), 
 			GFP_KERNEL);
 		AGV_CHECK_AND_GO(!newnode, allocfailed, "kmalloc failed!");
@@ -114,6 +126,7 @@ int domain_table_load(const struct domain_list_st* dlist)
 		list_add(&newnode->m_dnode, &newdht->m_htable[index].m_dnode);
 	}
 
+	// TODO: 换掉s_domain_htable
 	ret = 0;
 _done:
 	return ret;
@@ -125,7 +138,46 @@ _allocfailed:
 }
 
 /*
- * @brief: 域名列表初始化
+ * @brief: domain_match - 域名匹配函数，匹配域名是否命中
+ * @param: domain [in] 域名字符串
+ * @return: 命中返回1；不命中返回0；错误返回<0
+ */
+int domain_match(const char* domain)
+{
+	int matched = -1;
+	uint32_t domain_crc = 0;
+	size_t bucket_index = 0;
+	struct domain_htable* dhtable = NULL;
+	struct list_head* head = NULL;
+	struct domain_node* pos = NULL;
+
+	AGV_CHECK_AND_GO(!domain, done, "domain is NULL!\n");
+	domain_crc = crc32c(0, domain, strlen(domain));
+
+	/* need rcu lock here */
+	rcu_read_lock();
+	dhtable = rcu_dereference(s_domain_htable);
+	AGV_CHECK_AND_GO(!dhtable, unlock, "s_domain_htable is NULL!\n");
+	
+	bucket_index = domaincrc2bucket(domain_crc, dhtable->m_htable_size);
+	head = &dhtable->m_htable[bucket_index].m_dnode;
+
+	list_for_each_entry(pos, head, m_dnode) {
+		if (pos->m_domain_crc == domain_crc) {
+			matched = 1;
+			break;
+		}
+	}
+	matched = 0;
+
+_unlock:
+	rcu_read_unlock();
+_done:
+	return matched;
+}
+
+/*
+ * @brief: domain_table_init - 域名列表初始化
  * @return: 成功返回0；失败返回-1
  */
 int domain_table_init(void)
@@ -146,7 +198,7 @@ _done:
 }
 
 /*
- * @brief: 域名列表清理函数
+ * @brief: domain_table_cleanup - 域名列表清理函数
  * @return: 无
  */
 void domain_table_cleanup(void)
--- src/module/domain_table.h
+++ src/module/domain_table.h
@@ -4,33 +4,33 @@
 #include "agv_common.h"
 
 /*
- * @brief: 域名配置下发函数，更新域名列表
+ * @brief: domain_table_load - 域名配置下发函数，更新域名列表
  * @param: dlist [in] 由应用层下发的配置列表
  * @return: 成功返回0；失败返回<0
  */
 int domain_table_load(const struct domain_list_st* dlist);
 
 /*
- * @brief: 域名匹配函数，匹配域名是否命中
+ * @brief: domain_match - 域名匹配函数，匹配域名是否命中
  * @param: domain [in] 域名字符串
  * @return: 命中返回1；不命中返回0；错误返回<0
  */
 int domain_match(const char* domain);
 
 /*
- * @brief: 当前域名列表输出
+ * @brief: domain_table_show - 当前域名列表输出
  * @return: 成功返回0；失败返回<0
  */
 int domain_table_show(void);
 
 /*
- * @brief: 域名列表初始化
+ * @brief: domain_table_init - 域名列表初始化
  * @return: 成功返回0；失败返回-1
  */
 int domain_table_init(void);
 
 /*
- * @brief: 域名列表清理函数
+ * @brief: domain_table_cleanup - 域名列表清理函数
  * @return: 无
  */
 void domain_table_cleanup(void);
