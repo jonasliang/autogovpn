b092496357746f7b16514d2f498f2ce046e184b5
Jonas Liang <liangzijie@gmail.com>
2013-07-10 12:57:49 +0800

    fix forget-to-vfree problem and optimize some code

--- src/module/domain_table.c
+++ src/module/domain_table.c
@@ -12,12 +12,8 @@
 #define MIN_DOMAIN_BUCKETS 1024
 #define MAX_DOMAIN_BUCKETS 65535
 
-/* Could be modified while loading module with param */
-static size_t s_domain_htable_size = MIN_DOMAIN_BUCKETS;
-module_param(s_domain_htable_size, uint, 0644);
-
 struct domain_node {
-	struct list_head m_dnode;
+	struct list_head m_lnode;
 	uint32_t m_domain_crc;
 };
 
@@ -26,10 +22,14 @@ struct domain_htable {
 	struct domain_node* m_htable;
 };
 
+/* Could be modified while loading module with param */
+static size_t s_domain_htable_size = MIN_DOMAIN_BUCKETS;
+module_param(s_domain_htable_size, uint, 0644);
+
 static struct domain_htable* s_domain_htable = NULL;
 static DEFINE_SPINLOCK(s_domain_htable_lock); // lock only modify s_domain_htable
 
-inline static size_t domaincrc2bucket(uint32_t domain_crc, size_t htable_size)
+inline static size_t crc2index(uint32_t domain_crc, size_t htable_size)
 {
 	return domain_crc % htable_size;  // could be optimized
 }
@@ -41,32 +41,32 @@ inline static size_t domaincrc2bucket(uint32_t domain_crc, size_t htable_size)
  */
 static struct domain_htable* alloc_domain_htable(size_t htable_size)
 {
-	struct domain_htable* newdht = NULL;
-	struct domain_node* newtable = NULL;
-	size_t dt_size = sizeof(struct domain_node) * htable_size;
 	int i = 0;
+	struct domain_htable* newdht = NULL;
+	struct domain_node* newhtable = NULL;
+	size_t dt_size = sizeof(struct domain_node) * htable_size;
 
 	newdht = kmalloc(sizeof(struct domain_htable), GFP_KERNEL);
 	AGV_CHECK_AND_GO(!newdht, done, "kmalloc newdht failed!\n");
 
-	newtable = vmalloc(dt_size);
-	AGV_CHECK_AND_GO(!newtable, vfailed, \
+	newhtable = vmalloc(dt_size);
+	AGV_CHECK_AND_GO(!newhtable, vfailed, \
 		"vmalloc failed! total size = %u\n", dt_size);
 
-	memset(newtable, 0, dt_size);
+	memset(newhtable, 0, dt_size);
 
-	for (i = 0; i < htable_size; ++i) {
-		INIT_LIST_HEAD(&newtable[i].m_dnode);
-	}
+	for (i = 0; i < htable_size; ++i)
+		INIT_LIST_HEAD(&newhtable[i].m_lnode);
+
 	newdht->m_htable_size = htable_size;
-	newdht->m_htable = newtable;
+	newdht->m_htable = newhtable;
 
 _done:
 	return newdht;
 
 _vfailed:
 	kfree(newdht);
-	newdht = NULL;
+	newdht = (void*)POISON_FREE;
 	goto _done;
 }
 
@@ -86,18 +86,22 @@ static void free_domain_htable(struct domain_htable** pdhtable)
 	dhtable = *pdhtable;
 
 	for (i = 0; i < dhtable->m_htable_size; ++i) {
-		struct list_head* head = &dhtable->m_htable[i].m_dnode;
 		struct domain_node* pos = NULL;
 		struct domain_node* next = NULL;
+		struct list_head* head = &dhtable->m_htable[i].m_lnode;
 
-		list_for_each_entry_safe(pos, next, head, m_dnode) {
-			list_del(&pos->m_dnode);
+		list_for_each_entry_safe(pos, next, head, m_lnode) {
+			list_del(&pos->m_lnode);
 			kfree(pos);
+			pos = (void*)POISON_FREE;
 		}
 	}
 
+	vfree(dhtable->m_htable);
+	dhtable->m_htable = (void*)POISON_FREE;
+
 	kfree(dhtable);
-	*pdhtable = NULL;
+	*pdhtable = (void*)POISON_FREE;
 }
 
 /*
@@ -116,18 +120,17 @@ int domain_table_load(const struct domain_list_st* dlist)
 		done, "error! dlist is NULL or dlist size is 0!\n");
 
 	newdht = alloc_domain_htable(s_domain_htable_size);
-	AGV_CHECK_AND_GO(!newdht, done, "alloc new domain_bucket failed!\n");
+	AGV_CHECK_AND_GO(!newdht, done, "alloc failed!\n");
 
 	for (i = 0; i < dlist->m_size; ++i)	{
-		size_t index = domaincrc2bucket(dlist->m_domains_crc[i], 
-			s_domain_htable_size);
+		size_t index = crc2index(dlist->m_domains_crc[i], s_domain_htable_size);
 		struct domain_node* newnode = kmalloc(sizeof(struct domain_node), 
 			GFP_KERNEL);
-		AGV_CHECK_AND_GO(!newnode, allocfailed, "kmalloc failed!");
+		AGV_CHECK_AND_GO(!newnode, allocfailed, "kmalloc failed! i = %d\n", i);
 
 		newnode->m_domain_crc = dlist->m_domains_crc[i];
-		INIT_LIST_HEAD(&newnode->m_dnode);
-		list_add(&newnode->m_dnode, &newdht->m_htable[index].m_dnode);
+		INIT_LIST_HEAD(&newnode->m_lnode);
+		list_add(&newnode->m_lnode, &newdht->m_htable[index].m_lnode);
 	}
 
 	spin_lock(&s_domain_htable_lock);
@@ -160,9 +163,9 @@ int domain_match(const char* domain, size_t len)
 	int matched = -1;
 	uint32_t domain_crc = 0;
 	size_t bucket_index = 0;
-	struct domain_htable* dhtable = NULL;
 	struct list_head* head = NULL;
 	struct domain_node* pos = NULL;
+	struct domain_htable* dhtable = NULL;
 
 	AGV_CHECK_AND_GO((!domain || 0 == len), done, "domain is NULL!\n");
 	domain_crc = crc32(0, domain, len);
@@ -172,10 +175,10 @@ int domain_match(const char* domain, size_t len)
 	dhtable = rcu_dereference(s_domain_htable);
 	AGV_CHECK_AND_GO(!dhtable, unlock, NULL);
 	
-	bucket_index = domaincrc2bucket(domain_crc, dhtable->m_htable_size);
-	head = &dhtable->m_htable[bucket_index].m_dnode;
+	bucket_index = crc2index(domain_crc, dhtable->m_htable_size);
+	head = &dhtable->m_htable[bucket_index].m_lnode;
 
-	list_for_each_entry(pos, head, m_dnode) {
+	list_for_each_entry(pos, head, m_lnode) {
 		if (pos->m_domain_crc == domain_crc) {
 			matched = 1;
 			break;
@@ -223,7 +226,8 @@ void domain_table_cleanup(void)
 
 	spin_lock(&s_domain_htable_lock);
 	tmpdht = rcu_dereference(s_domain_htable);
-	rcu_assign_pointer(s_domain_htable, NULL);
+	/* assign POISON_FREE - just go to hell if somewhere use it after cleanup */
+	rcu_assign_pointer(s_domain_htable, (void*)POISON_FREE);
 	spin_unlock(&s_domain_htable_lock);
 
 	synchronize_rcu();
