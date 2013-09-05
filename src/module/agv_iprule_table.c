d5abb744aa55f0bb6f13d63cb278591ae70a80eb
Jonas Liang <liangzijie@gmail.com>
2013-09-05 19:00:41 +0800

    实现iprule插入和查询

--- src/module/agv_iprule_table.c
+++ src/module/agv_iprule_table.c
@@ -1,8 +1,11 @@
-﻿#include <linux/mm.h>
+﻿#include <linux/random.h>
+#include <linux/jhash.h>
+#include <linux/mm.h>
 #include <linux/vmalloc.h>
 #include <linux/list.h>
 #include <linux/spinlock_types.h>
 #include <net/netfilter/nf_conntrack.h>
+#include <asm/cmpxchg.h>
 #include "agv_debug.h"
 #include "agv_iprule_table.h"
 
@@ -19,6 +22,105 @@ struct agv_iprule_table {
 };
 
 struct agv_iprule_table s_iprule_table;
+static uint32_t s_iprule_hash_rnd;
+
+static inline void iprule_hash_rnd_init(void)
+{
+	uint32_t rand;
+	do {
+		get_random_bytes(&rand, sizeof(rand));
+	} while(!rand);
+	cmpxchg(&s_iprule_hash_rnd, 0, rand);
+}
+
+static inline uint32_t hashiprule(const struct agv_iprule* iprule)
+{
+	return jhash_2words(iprule->m_srcip, iprule->m_dstip, 
+			s_iprule_hash_rnd);
+}
+
+/*
+ * @brief: iprule_table_insert - 插入一条iprule规则
+ * @param: iprule [in] - iprule规则
+ * @return: 成功返回0；失败返回-1
+ */
+int iprule_table_insert(const struct agv_iprule* iprule)
+{
+	int ret = -1;
+	int index = 0;
+	int exist = 0;
+	struct agv_iprule_node* pos = NULL;
+	struct list_head* head = NULL;
+
+	AGV_CHECK_AND_GO(!iprule, done, "invalid param.\n");
+
+	if (!s_iprule_table.m_enable)
+		goto _done;
+
+	index = hashiprule(iprule) % s_iprule_table.m_iprule_htsize;
+
+	spin_lock_bh(&s_iprule_table.m_iprule_locks[index]);
+	
+	// find the matched rule first, if exist, go out.
+	head = &s_iprule_table.m_iprule_htable[index].m_irnode;
+	list_for_each_entry(pos, head, m_irnode) {
+		if ( iprule_cmp(&pos->m_iprule, iprule) ) {
+			exist = 1;
+			break;
+		}
+	}
+
+	if (!exist) {
+		struct agv_iprule_node* newnode = kmalloc(sizeof(struct agv_iprule_node), 
+				GFP_ATOMIC);
+		AGV_CHECK_AND_GO(!newnode, unlock, "kmalloc agv_iprule_node failed.\n");
+		
+		INIT_LIST_HEAD(&newnode->m_irnode);
+		iprule_assign(&newnode->m_iprule, iprule);
+		list_add(head, &newnode->m_irnode);
+	}
+
+_unlock:
+	spin_unlock_bh(&s_iprule_table.m_iprule_locks[index]);
+
+_done:
+	return ret;
+}
+
+/*
+ * @brief: iprule_table_match - 匹配一条iprule规则
+ * @param: iprule [in] - iprule规则
+ * @return: 命中返回1；不命中返回0；错误返回<0
+ */
+int iprule_table_match(const struct agv_iprule* iprule)
+{
+	int found = -1;
+	int index = 0;
+	int exist = 0;
+	struct agv_iprule_node* pos = NULL;
+	struct list_head* head = NULL;
+
+	AGV_CHECK_AND_GO(!iprule, done, "invalid param.\n");
+	if (!s_iprule_table.m_enable)
+		goto _done;
+
+	index = hashiprule(iprule) % s_iprule_table.m_iprule_htsize;
+	found = 0;
+	spin_lock_bh(&s_iprule_table.m_iprule_locks[index]);
+	
+	// find the matched rule first, if exist, go out.
+	head = &s_iprule_table.m_iprule_htable[index].m_irnode;
+	list_for_each_entry(pos, head, m_irnode) {
+		if ( iprule_cmp(&pos->m_iprule, iprule) ) {
+			found = 1;
+			break;
+		}
+	}
+	spin_unlock_bh(&s_iprule_table.m_iprule_locks[index]);
+
+_done:
+	return found;
+}
 
 /*
  * @brief: iprule_table_init - ip规则列表初始化
@@ -53,6 +155,8 @@ int iprule_table_init(void)
 	s_iprule_table.m_iprule_htable = ipruleht;
 	s_iprule_table.m_iprule_locks = iprulelks;
 	s_iprule_table.m_enable = 1;
+	
+	iprule_hash_rnd_init();
 
 	ret = 0;
 
--- src/module/agv_iprule_table.h
+++ src/module/agv_iprule_table.h
@@ -6,10 +6,42 @@
 /* iprule规则，主机字节序 */
 struct agv_iprule {
 	uint32_t m_srcip;
-	uint32_t m_sipmask;
 	uint32_t m_dstip;
-	uint32_t m_dipmask;
 };
+
+/*
+ * @brief: iprule_cmp - for iprule struct compare
+ */
+static inline bool iprule_cmp(const struct agv_iprule* a, 
+		const struct agv_iprule* b)
+{
+	return (a->m_srcip == b->m_srcip && 
+			a->m_dstip == b->m_dstip);
+}
+
+/*
+ * @brief: iprule_assign - for iprule struct assignment
+ */
+static inline void iprule_assign(struct agv_iprule* to, 
+		const struct agv_iprule* from)
+{
+	to->m_srcip = from->m_srcip;
+	to->m_dstip = from->m_dstip;
+}
+
+/*
+ * @brief: iprule_table_insert - 插入一条iprule规则
+ * @param: iprule [in] - iprule规则
+ * @return: 成功返回0；失败返回-1
+ */
+int iprule_table_insert(const struct agv_iprule* iprule);
+
+/*
+ * @brief: iprule_table_match - 匹配一条iprule规则
+ * @param: iprule [in] - iprule规则
+ * @return: 命中返回1；不命中返回0；错误返回<0
+ */
+int iprule_table_match(const struct agv_iprule* iprule);
 
 /*
  * @brief: iprule_table_init - ip规则列表初始化
